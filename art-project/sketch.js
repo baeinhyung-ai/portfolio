aea7590526e86797300b32fa502fc66a120ca9ed
baeinhyung-ai <baeinhyung@gmail.com>
2026-03-12 11:11:04 +0900

    Update sketch.js

--- art-project/sketch.js
+++ art-project/sketch.js
@@ -1,144 +1,116 @@
 let stems = [];
 let particles = [];
-let colors = ['#FF007F', '#00FF00', '#0000FF', '#FFFF00', '#FF7F00', '#7F00FF']; // 무라카미 컬러셋
+let monogramColors = ['#FF45D1', '#45FF70', '#458BFF', '#FFD145', '#FF7A45']; // 무라카미 컬러 팔레트
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // 줄기 3개를 화면 하단에 일정 간격으로 배치
+  // 영상처럼 수직으로 뻗은 가느다란 줄기 3개 배치
   for (let i = 0; i < 3; i++) {
-    let x = width * (0.25 + i * 0.25);
-    stems.push(new Stem(x));
+    stems.push(new Stem(width * (0.25 + i * 0.25)));
   }
 }
 
 function draw() {
-  background(240); // 영상과 같은 밝은 배경
+  background(248); // 밝은 오프화이트 배경
   
-  // 상단 텍스트 레이아웃
-  drawBrandText();
+  // 텍스트 배치 (영상 상단 레이아웃)
+  drawUI();
 
-  // 줄기 업데이트 및 출력
+  // 줄기 그리기
   for (let s of stems) {
     s.update();
     s.show();
   }
 
-  // 입자 업데이트 및 출력
+  // 파티클 처리
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
     particles[i].show();
-    if (particles[i].isDead()) {
-      particles.splice(i, 1);
-    }
+    if (particles[i].isDead()) particles.splice(i, 1);
   }
 }
 
-// 클릭 시 가장 가까운 줄기 끝에서 파티클 폭발
 function mousePressed() {
-  let closestStem = stems[0];
-  let minDist = dist(mouseX, mouseY, stems[0].tip.x, stems[0].tip.y);
-  
+  // 클릭하면 가장 가까운 줄기 끝에서 파티클 팡!
+  let target = stems[0];
+  let dMin = dist(mouseX, mouseY, stems[0].tip.x, stems[0].tip.y);
   for (let s of stems) {
     let d = dist(mouseX, mouseY, s.tip.x, s.tip.y);
-    if (d < minDist) {
-      minDist = d;
-      closestStem = s;
-    }
+    if (d < dMin) { dMin = d; target = s; }
   }
-  
-  // 모노그램 모양으로 파티클 생성
-  let color = random(colors);
-  for (let i = 0; i < 60; i++) {
-    particles.push(new MonogramPoint(closestStem.tip.x, closestStem.tip.y, color));
+
+  let col = random(monogramColors);
+  for (let i = 0; i < 80; i++) {
+    particles.push(new DotParticle(target.tip.x, target.tip.y, col));
   }
 }
 
-function drawBrandText() {
+function drawUI() {
+  textAlign(CENTER);
   fill(0);
   noStroke();
-  textAlign(CENTER);
-  textFont('Helvetica');
-  textSize(24);
-  text('L O U I S   V U I T T O N', width / 2, 60);
+  textSize(22);
+  text('L O U I S   V U I T T O N', width / 2, height * 0.1);
   textSize(10);
-  fill(100);
-  text('MURAKAMI ULTRA-FINE EDITION / TACTILE STILLNESS', width / 2, 85);
+  fill(150);
+  text('MURAKAMI ULTRA-FINE EDITION / TACTILE STILLNESS', width / 2, height * 0.13);
 }
 
-// --- 가느다란 줄기 클래스 ---
 class Stem {
   constructor(x) {
     this.baseX = x;
-    this.tip = createVector(x, height * 0.3);
-    this.off = random(1000);
+    this.tip = createVector(x, height * 0.35);
+    this.noiseOff = random(1000);
   }
-
   update() {
-    // 마우스 위치에 따라 부드럽게 흔들림
-    let targetX = this.baseX + map(noise(this.off + frameCount * 0.01), 0, 1, -30, 30);
-    this.tip.x = lerp(this.tip.x, targetX, 0.1);
-    this.off += 0.01;
+    // 줄기가 아주 미세하게 살랑거리는 움직임
+    let sw = sin(frameCount * 0.02 + this.noiseOff) * 15;
+    this.tip.x = this.baseX + sw;
   }
-
   show() {
     stroke(0);
-    strokeWeight(3);
+    strokeWeight(1.5); // 영상처럼 아주 가늘게
     noFill();
-    // 부드러운 곡선 줄기
     beginShape();
     vertex(this.baseX, height);
-    bezierVertex(this.baseX, height * 0.7, this.tip.x, height * 0.5, this.tip.x, this.tip.y);
+    bezierVertex(this.baseX, height * 0.7, this.tip.x, height * 0.6, this.tip.x, this.tip.y);
     endShape();
 
-    // 줄기 끝의 발광 구체 (Blur 효과 대용)
+    // 줄기 끝 발광체
+    fill(0, 20);
     noStroke();
-    for (let i = 10; i > 0; i--) {
-      fill(0, 10);
-      ellipse(this.tip.x, this.tip.y, i * 8);
-    }
-    fill(200);
-    ellipse(this.tip.x, this.tip.y, 15);
+    ellipse(this.tip.x, this.tip.y, 40, 40);
+    fill(100);
+    ellipse(this.tip.x, this.tip.y, 10, 10);
   }
 }
 
-// --- 루이비통 도트 파티클 클래스 ---
-class MonogramPoint {
+class DotParticle {
   constructor(x, y, col) {
     this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D().mult(random(1, 4));
-    this.acc = createVector(0, -0.02); // 위로 살짝 떠오르는 효과
+    this.vel = p5.Vector.random2D().mult(random(1, 5));
     this.color = col;
     this.lifespan = 255;
-    this.size = random(2, 4);
-    
-    // 무라카미 꽃 모양의 좌표 오프셋 계산 (수학적 형태)
-    this.shapeOffset = this.calculateMonogram();
-  }
-
-  calculateMonogram() {
+    // LV 꽃 모양 오프셋 (수학적 형태)
     let angle = random(TWO_PI);
-    // 루이비통 꽃잎 4개 공식: r = cos(2 * theta)
-    let r = 40 * cos(2 * angle); 
-    return createVector(cos(angle) * r, sin(angle) * r);
+    let r = 50 * cos(2 * angle); // 4잎 꽃 형태 공식
+    this.targetOffset = createVector(cos(angle) * r, sin(angle) * r);
   }
-
   update() {
     this.pos.add(this.vel);
-    this.vel.add(this.acc);
-    this.lifespan -= 2;
+    this.vel.mult(0.96); // 서서히 멈춤
+    this.lifespan -= 3;
   }
-
   show() {
     noStroke();
     let c = color(this.color);
     c.setAlpha(this.lifespan);
     fill(c);
-    // 중심점 주변으로 형태를 이루며 퍼짐
-    ellipse(this.pos.x + this.shapeOffset.x * map(this.lifespan, 255, 0, 0, 2), 
-            this.pos.y + this.shapeOffset.y * map(this.lifespan, 255, 0, 0, 2), 
-            this.size);
+    // 도트들이 꽃 모양을 형성하며 퍼지도록 함
+    let drawX = this.pos.x + this.targetOffset.x * map(this.lifespan, 255, 0, 0, 1.5);
+    let drawY = this.pos.y + this.targetOffset.y * map(this.lifespan, 255, 0, 0, 1.5);
+    ellipse(drawX, drawY, 3, 3);
   }
-
   isDead() { return this.lifespan < 0; }
 }
 
