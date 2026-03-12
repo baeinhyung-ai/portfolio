44e1b6e8594303da7c67ba1cec21a2d411c7f566
baeinhyung-ai <baeinhyung@gmail.com>
2026-03-12 11:15:22 +0900

    Update sketch.js

--- art-project/sketch.js
+++ art-project/sketch.js
@@ -1,28 +1,24 @@
 let stems = [];
 let particles = [];
-let monogramColors = ['#FF45D1', '#45FF70', '#458BFF', '#FFD145', '#FF7A45']; // 무라카미 컬러 팔레트
+// 무라카미 다카시 특유의 비비드한 컬러셋
+let monogramColors = ['#FF45D1', '#45FF70', '#458BFF', '#FFD145', '#FF7A45', '#BA45FF']; 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // 영상처럼 수직으로 뻗은 가느다란 줄기 3개 배치
   for (let i = 0; i < 3; i++) {
     stems.push(new Stem(width * (0.25 + i * 0.25)));
   }
 }
 
 function draw() {
-  background(248); // 밝은 오프화이트 배경
-  
-  // 텍스트 배치 (영상 상단 레이아웃)
+  background(248); 
   drawUI();
 
-  // 줄기 그리기
   for (let s of stems) {
     s.update();
     s.show();
   }
 
-  // 파티클 처리
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
     particles[i].show();
@@ -31,7 +27,6 @@ function draw() {
 }
 
 function mousePressed() {
-  // 클릭하면 가장 가까운 줄기 끝에서 파티클 팡!
   let target = stems[0];
   let dMin = dist(mouseX, mouseY, stems[0].tip.x, stems[0].tip.y);
   for (let s of stems) {
@@ -39,9 +34,9 @@ function mousePressed() {
     if (d < dMin) { dMin = d; target = s; }
   }
 
-  let col = random(monogramColors);
-  for (let i = 0; i < 80; i++) {
-    particles.push(new DotParticle(target.tip.x, target.tip.y, col));
+  // 클릭 시 30개의 모노그램 파티클 생성
+  for (let i = 0; i < 30; i++) {
+    particles.push(new MonogramParticle(target.tip.x, target.tip.y));
   }
 }
 
@@ -50,9 +45,11 @@ function drawUI() {
   fill(0);
   noStroke();
   textSize(22);
+  textFont('Georgia'); // 약간 더 고급스러운 폰트
   text('L O U I S   V U I T T O N', width / 2, height * 0.1);
   textSize(10);
   fill(150);
+  textFont('Helvetica');
   text('MURAKAMI ULTRA-FINE EDITION / TACTILE STILLNESS', width / 2, height * 0.13);
 }
 
@@ -63,54 +60,90 @@ class Stem {
     this.noiseOff = random(1000);
   }
   update() {
-    // 줄기가 아주 미세하게 살랑거리는 움직임
     let sw = sin(frameCount * 0.02 + this.noiseOff) * 15;
     this.tip.x = this.baseX + sw;
   }
   show() {
     stroke(0);
-    strokeWeight(1.5); // 영상처럼 아주 가늘게
+    strokeWeight(1.2); 
     noFill();
     beginShape();
     vertex(this.baseX, height);
     bezierVertex(this.baseX, height * 0.7, this.tip.x, height * 0.6, this.tip.x, this.tip.y);
     endShape();
 
-    // 줄기 끝 발광체
-    fill(0, 20);
+    fill(0, 15);
     noStroke();
-    ellipse(this.tip.x, this.tip.y, 40, 40);
-    fill(100);
-    ellipse(this.tip.x, this.tip.y, 10, 10);
+    ellipse(this.tip.x, this.tip.y, 40);
+    fill(180);
+    ellipse(this.tip.x, this.tip.y, 8);
   }
 }
 
-class DotParticle {
-  constructor(x, y, col) {
+class MonogramParticle {
+  constructor(x, y) {
     this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D().mult(random(1, 5));
-    this.color = col;
+    this.vel = p5.Vector.random2D().mult(random(2, 6));
+    this.color = random(monogramColors);
     this.lifespan = 255;
-    // LV 꽃 모양 오프셋 (수학적 형태)
-    let angle = random(TWO_PI);
-    let r = 50 * cos(2 * angle); // 4잎 꽃 형태 공식
-    this.targetOffset = createVector(cos(angle) * r, sin(angle) * r);
+    this.size = random(10, 20);
+    this.type = floor(random(4)); // 0: 둥근꽃, 1: 별모양, 2: LV, 3: 원형꽃
+    this.rot = random(TWO_PI);
+    this.rotVel = random(-0.05, 0.05);
   }
+
   update() {
     this.pos.add(this.vel);
-    this.vel.mult(0.96); // 서서히 멈춤
+    this.vel.mult(0.97); // 부드럽게 감속
+    this.rot += this.rotVel;
     this.lifespan -= 3;
   }
+
   show() {
-    noStroke();
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.rot);
     let c = color(this.color);
     c.setAlpha(this.lifespan);
-    fill(c);
-    // 도트들이 꽃 모양을 형성하며 퍼지도록 함
-    let drawX = this.pos.x + this.targetOffset.x * map(this.lifespan, 255, 0, 0, 1.5);
-    let drawY = this.pos.y + this.targetOffset.y * map(this.lifespan, 255, 0, 0, 1.5);
-    ellipse(drawX, drawY, 3, 3);
+    stroke(c);
+    strokeWeight(1.5);
+    noFill();
+
+    // 루이비통 모노그램 타입별 그리기
+    if (this.type === 0) {
+      // 1. 둥근 4잎 꽃 (Rounded Flower)
+      for (let i = 0; i < 4; i++) {
+        rotate(HALF_PI);
+        ellipse(this.size * 0.4, 0, this.size * 0.8, this.size * 0.5);
+      }
+    } else if (this.type === 1) {
+      // 2. 뾰족한 4잎 별 (Pointed Flower / Star)
+      beginShape();
+      for (let i = 0; i < 8; i++) {
+        let r = i % 2 === 0 ? this.size : this.size * 0.3;
+        let x = cos(QUARTER_PI * i) * r;
+        let y = sin(QUARTER_PI * i) * r;
+        vertex(x, y);
+      }
+      endShape(CLOSE);
+    } else if (this.type === 2) {
+      // 3. LV 로고 텍스트
+      noStroke();
+      fill(c);
+      textSize(this.size);
+      textAlign(CENTER, CENTER);
+      text('LV', 0, 0);
+    } else {
+      // 4. 원 안의 꽃 (Circle Flower)
+      ellipse(0, 0, this.size * 1.5);
+      for (let i = 0; i < 4; i++) {
+        rotate(HALF_PI);
+        ellipse(this.size * 0.3, 0, this.size * 0.6, this.size * 0.3);
+      }
+    }
+    pop();
   }
+
   isDead() { return this.lifespan < 0; }
 }
 
